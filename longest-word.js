01743458a924b1b87040a780216350c3be3e2159
Cool-Runningz <6391149+Cool-Runningz@users.noreply.github.com>
2018-08-26 16:00:27 -0400

    Create longest-word.js

---
+++ longest-word.js
@@ -0,0 +1,30 @@
+/*
+Using the JavaScript language, have the function LongestWord(sen) take the sen parameter being passed and return the largest 
+word in the string. If there are two or more words that are the same length, return the first word from the string with that
+length. Ignore punctuation and assume sen will not be empty. 
+*/
+
+function LongestWord(sen) { 
+
+
+var nextWord = "";
+var patt1 = /[A-Za-z]/g;
+
+var strArr = sen.split(" ");
+
+var longestWord = strArr[0].match(patt1).join("");
+
+if(strArr.length > 1){
+    
+    for(var i = 1; i < strArr.length; i++){
+        nextWord = strArr[i].match(patt1).join("");
+        
+        if(nextWord.length > longestWord.length){
+            longestWord = nextWord;
+        }
+    }
+}
+
+  return longestWord; 
+         
+} 
