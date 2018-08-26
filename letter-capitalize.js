81523b5bc6230f162da7eebcf955703aac1ae4a3
Cool-Runningz <6391149+Cool-Runningz@users.noreply.github.com>
2018-08-26 16:14:40 -0400

    revising description for letter-capitalize

--- letter-capitalize.js
+++ letter-capitalize.js
@@ -1,6 +1,15 @@
 /*
- Using the JavaScript language, have the function LetterCapitalize(str) take the str parameter being passed and capitalize 
- the first letter of each word. Words will be separated by only one space. 
+ Using the JavaScript language, have the function LetterCapitalize(str) take the str parameter being passed and capitalize the 
+ first letter of each word. Words will be separated by only one space. 
+ 
+Sample Test Cases
+-------------------
+Input: "hello world"
+Output: "Hello World"
+
+Input: "i ran there"
+Output: "I Ran There"
+
 */
 
 function LetterCapitalize(str) { 
